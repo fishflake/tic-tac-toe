1da07b0e6b87fca136e1a09bba19ddc19338bf04
rachciav <rachel.ciavarella@gmail.com>
2018-07-14 09:37:51 -0400

    fixed the file

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,18 @@
-print "hello world"
+#tic-tac-toe game program by Rachel Ciavarella 2018 
+#for Pair Programming task the Recurse Center Applicaiton
+
+#Before your interview, write a program that lets two humans play a game of Tic Tac Toe in a terminal. 
+#The program should let the players take turns to input their moves. 
+#The program should report the outcome of the game.
+
+
+#Begin by drawing the playing board in the terminal window so the human player can see it
+print " 1 | 2 | 3 "
+print "-----------"
+print " 4 | 5 | 6 "
+print "-----------"
+print " 7 | 8 | 9 "
+
+print "Player X make your move"
+
+#players make a move by selecting one of the numbered sections to enter their move
